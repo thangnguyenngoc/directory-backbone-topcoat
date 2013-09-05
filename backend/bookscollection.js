4c0607c5afd2e51f57812eadf157881c52738dda
Atemix <contactus@atemix.com>
2013-09-05 18:34:15 +0700

    add kinvey code to init of home

---
+++ backend/bookscollection.js
@@ -0,0 +1,17 @@
+[
+	{"id": 1, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/8/2013"},
+	{"id": 2, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/9/2013"},
+	{"id": 3, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/10/2013"},
+	{"id": 4, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/11/2013"},
+	{"id": 5, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/12/2013"},
+	{"id": 6, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/13/2013"},
+	{"id": 7, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/14/2013"},
+	{"id": 8, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/15/2013"},
+	{"id": 9, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/16/2013"},
+	{"id": 10, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/17/2013"},
+	{"id": 11, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/18/2013"},
+	{"id": 12, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/19/2013"},
+	{"id": 13, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/20/2013"},
+	{"id": 14, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/21/2013"},
+	{"id": 15, "title": "Hello World", "author": "unknown", "category": "fairy tale", "publisheddate": "8/22/2013"}
+]
--- js/views/home.js
+++ js/views/home.js
@@ -1,6 +1,24 @@
 app.views.home = Backbone.View.extend({
 
     initialize: function () {
+		window.KINVEY_DEBUG = true;
+		Kinvey.init({
+			appKey: "kid_VeHZ4rGBeO",
+			appSecret: "bcdeef3788584ea4813c0dce76f88e5f"
+		})
+		.then(function (activeUser) {
+			// the Kinvey.init function returns a promise which resolves to the active user
+			// data (or null if there is no active user). Note: when logged in, activeUser
+			// here is *not* an instance of Kinvey.Backbone.User, but just the attributes of
+			// the user. You must instantiate the User yourself (to allow for custom subclasses).
+			//App.user = new Kinvey.Backbone.User(activeUser);
+			var user = new Kinvey.Backbone.User();
+			var promise = user.connect('facebook', {
+				success: function(model, response, options) {
+					//do nothing
+				}
+			});
+		};
     },
 
     render: function () {
